1ad2aeb028093ebb942a840692aaa02c8247f8f8
Gustavo Valbuena <gust@Gustavos-MacBook-Air.local>
2018-11-25 18:16:41 -0300

    creating candidate model and add candidate function

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -1,10 +1,25 @@
 pragma solidity ^0.4.24;
 
 contract Election {
-    string public candidate;
-    //constructor
+    //Model of a candidate
+    struct Candidate {
+        uint id;
+        string name;
+        uint voteCount;
+    }
+    //Store Candidate
+    mapping(uint => Candidate) public candidates;
+    //Store Candidates Count
+    uint public candidatesCount;
+    //constuctor
     constructor () public {
-        candidate = "Candidate 1";
+        addCandidate("candidate 1");
+        addCandidate("candidate 2");
+    }
+
+    function addCandidate (string _name) private {
+        candidatesCount ++;
+        candidates[candidatesCount] = Candidate(candidatesCount, _name, 0);
     }
 }
 
